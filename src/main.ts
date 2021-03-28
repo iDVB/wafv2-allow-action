ebbe56e9d7868a1b3036305460d0c67f2ab14067
iDVB <me@danvanbrunt.com>
2021-03-27 22:44:15 -0400

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,107 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+// import {wait} from './wait'
+import AWS from 'aws-sdk'
+import {HttpClient} from '@actions/http-client'
+import {without as _without} from 'lodash'
+
+let wafv2: AWS.WAFV2
+
+type IPSetInfo = {
+  Id: string
+  Name: string
+  Scope: string
+}
+
+/**
+ * IPify Response.
+ *
+ * @see https://www.ipify.org/
+ */
+interface IPResponse {
+  ip: string
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const action: string = core.getInput('action') || 'add'
+    const region: string = core.getInput('region') || 'us-east-1'
+    wafv2 = new AWS.WAFV2({apiVersion: '2019-07-29', region})
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const IPSetInputs = getIPSetFromInputs()
+    const myIp = await getMyIp()
+    const NewAddress = `${myIp}/32`
 
-    core.setOutput('time', new Date().toTimeString())
+    if (action === 'add') {
+      await addIP(NewAddress, IPSetInputs)
+    } else if (action === 'remove') {
+      await removeIP(NewAddress, IPSetInputs)
+    }
+
+    core.setOutput('address', NewAddress)
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+const getMyIp = async (): Promise<string> => {
+  const http = new HttpClient('haythem/public-ip', undefined, {
+    allowRetries: true,
+    maxRetries: 10
+  })
+  const ipv4 = await http.getJson<IPResponse>(
+    'https://api.ipify.org?format=json'
+  )
+  const ip = ipv4?.result?.ip
+  if (!ip) throw new Error(`Can't Determine runner IP`)
+
+  core.info(`ipv4: ${ip}`)
+  return ip
+}
+
+const getIPSetFromInputs = (): IPSetInfo => {
+  const Id: string = core.getInput('ipset_id')
+  const Name: string = core.getInput('ipset_name')
+  const Scope: string = core.getInput('ipset_scope')
+
+  return {Id, Name, Scope}
+}
+
+const addIP = async (
+  AddAddress: string,
+  IPSetParams: IPSetInfo
+): Promise<void> => {
+  const {IPSet, LockToken} = await wafv2.getIPSet(IPSetParams).promise()
+  if (!IPSet || !LockToken) throw new Error('IPSet and/or LockToken Not Found.')
+
+  const {Addresses: CurrentAddresses} = IPSet
+  const Addresses = [...CurrentAddresses, AddAddress]
+
+  await wafv2
+    .updateIPSet({
+      Addresses,
+      LockToken,
+      ...IPSetParams
+    })
+    .promise()
+}
+
+const removeIP = async (
+  RemoveAddress: string,
+  IPSetParams: IPSetInfo
+): Promise<void> => {
+  const {IPSet, LockToken} = await wafv2.getIPSet(IPSetParams).promise()
+  if (!IPSet || !LockToken) throw new Error('IPSet and/or LockToken Not Found.')
+
+  const {Addresses: CurrentAddresses} = IPSet
+  const Addresses = _without(CurrentAddresses, RemoveAddress)
+
+  await wafv2
+    .updateIPSet({
+      Addresses,
+      LockToken,
+      ...IPSetParams
+    })
+    .promise()
 }
 
 run()
